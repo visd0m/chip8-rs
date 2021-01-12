70cca42972a47f86a4e1de80ab03eddad42e6f81
Domenico Visconti <domenico.visconti@prima.it>
2021-01-12 22:38:53 +0100

    add tetris rom, add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## Chip-8 emulator
+
+Chip-8 emulator implementation in rust.
+
+[Hardware specifications](http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.1).
+
+#### Todos
+
+- [ ] menu to select rom
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ mod emulator;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut rom = Vec::new();
     // let mut file = File::open(Path::new("./Brix [Andreas Gustafsson, 1990].ch8"))?;
-    let mut file = File::open(Path::new("./Breakout (Brix hack) [David Winter, 1997].ch8"))?;
+    // let mut file = File::open(Path::new("./Breakout (Brix hack) [David Winter, 1997].ch8"))?;
     // let mut file = File::open(Path::new("./Pong (alt).ch8"))?;
     // let mut file = File::open(Path::new("./test_opcode.ch8"))?;
+    let mut file = File::open(Path::new("./Tetris [Fran Dachille, 1991].ch8"))?;
     file.read_to_end(&mut rom)?;
 
     let mut emu = Emulator::new(&rom)?;
